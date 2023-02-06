0f482bea6f083480b2c43790a1171ef47f431a41
Jochem van der Steen <jochemvdsteen@kpnmail.nl>
2023-02-06 19:30:12 +0100

    move more

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [SerializeField] private float mouseSensitivity = 1000f;
+    public float mouseSensitivity = 100f;
     public Transform playerBody;
     float xRotation = 0f;
 
